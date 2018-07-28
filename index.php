917756ba85313fd321dd7e42582eb6d29c84d64c
Dev Singh <devsingh120397@gmail.com>
2018-07-27 22:17:14 -0700

    new changes

--- index.php
+++ index.php
@@ -18,31 +18,7 @@
             <?php
             $query = "SELECT * FROM posts";
             $result = mysqli_query($connection,$query);
-            while($row = mysqli_fetch_assoc($result)){
-                $title = $row["post_title"];
-                $author = $row["post_author"];
-                $date = $row["post_date"];
-                $image = $row["post_image"];
-                $content = $row["post_content"];
-                $tags= $row["post_tags"];
-                ?>
-                    <!-- First Blog Post -->
-                    <h2>
-                        <a href="#"><?php echo $title ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php  echo $author ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span><?php echo " ".$date ?></p>
-                    <hr>
-                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                    <hr>
-                    <p><?php  echo $content ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                    <hr>
-                <?php
-            }
+            print_query($result);
             ?>
             </div>
             <!-- Blog Sidebar Widgets Column -->
@@ -50,20 +26,31 @@
         </div>
         <!-- /.row -->
         <hr>
-
 <?php
-include "includes/footer.php";
+function print_query($result){
+    while($row = mysqli_fetch_assoc($result)){
+        $title = $row["post_title"];
+        $author = $row["post_author"];
+        $date = $row["post_date"];
+        $image = $row["post_image"];
+        $content = $row["post_content"];
+        $tags= $row["post_tags"];
+        ?>
+        <h2>
+            <a href="#"><?php echo $title ?></a>
+        </h2>
+        <p class="lead">
+            by <a href="index.php"><?php  echo $author ?></a>
+        </p>
+        <p><span class="glyphicon glyphicon-time"></span><?php echo " ".$date ?></p>
+        <hr>
+        <img class="img-responsive" src="./Images/<?php echo $image?>" alt="">
+        <hr>
+        <p><?php  echo $content ?></p>
+        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <hr>
+        <?php
+    }
+}
 ?>
-
-
-
-<!---->
-<!--        <!-- Pager -->-->
-<!--        <ul class="pager">-->
-<!--            <li class="previous">-->
-<!--                <a href="#">&larr; Older</a>-->
-<!--            </li>-->
-<!--            <li class="next">-->
-<!--                <a href="#">Newer &rarr;</a>-->
-<!--            </li>-->
-<!--        </ul>-->
+<?php include "includes/footer.php"; ?>
