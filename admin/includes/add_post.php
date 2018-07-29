20c1bb67757dd584e76a420976727c2aed3fa7e8
Dev Singh <devsingh120397@gmail.com>
2018-07-28 20:32:02 -0700

    Adding posts functionalit

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,13 +1,42 @@
-<form action="" method="post">
+<?php
+
+if(isset($_POST['add_post'])){
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+
+    $date = $_POST['date'];
+    $content = $_POST['content'];
+    $tags = $_POST['tags'];
+
+    $image = $_FILES['image']['name'];
+    $temp_image= $_FILES['image']['tmp_name'];
+
+    move_uploaded_file($temp_image,"../Images/".$image);
+
+    $query = "INSERT INTO posts (post_title,post_author,post_date,post_image,post_content,post_tags) VALUES ('$title','$author','$date','$image','$content','$tags')";
+    $result = mysqli_query($connection,$query);
+    if($result){
+        echo "yay";
+    }
+    else{
+        die(mysqli_error($connection));
+    }
+}
+
+?>
+<form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
         <input type="text" placeholder="Enter Title" name="title" class="form-control">
+
+        <label for="author">Post Title</label>
+        <input type="text" placeholder="Enter Author" name="author" class="form-control">
 
         <label for="date">Post Date</label>
         <input type="date" placeholder="Enter Title" name="date" class="form-control">
 
         <label for="date">Image</label>
-        <input type="file" name="pic" accept="image/*" class="form-control" name="image ">
+        <input type="file" accept="image/*" class="form-control" name="image">
 
         <label for="date">Post Date</label>
         <textarea class="form-control text" placeholder="Enter Content" name="content"></textarea>
@@ -15,7 +44,6 @@
         <label for="tags">Post Tags</label>
         <input type="text" placeholder="Enter Title" name="tags" class="form-control">
 
-        <button type="button" class="btn btn-dark">Save</button>
-
+        <button type="submit" class="btn btn-dark" name="add_post">Save</button>
     </div>
 </form>
--- admin/includes/sidebar_admin.php
+++ admin/includes/sidebar_admin.php
@@ -1,26 +1,26 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-        <li class="active">
+        <li class="menu-options">
             <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
-        <li>
+        <li class="menu-options">
             <a href="javascript:;" data-toggle="collapse" data-target="#post"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="post" class="collapse">
 
-                <li>
+                <li class="menu-options">
                     <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> View All Posts</a>
                     <a href="post.php"><i class="glyphicon glyphicon-plus"></i>  Add Post</a>
 
                 </li>
             </ul>
         </li>
-        <li>
+        <li class="menu-options">
             <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
         </li>
-        <li>
+        <li class="menu-options">
             <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
         </li>
-        <li>
+        <li class="menu-options">
             <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo" class="collapse">
                 <li>
@@ -32,7 +32,7 @@
             </ul>
         </li>
 
-        <li>
+        <li class="menu-options">
             <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
         </li>
     </ul>
