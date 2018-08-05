52d1ebba1d43c318015c60580377aa0af1cffab2
Dev Singh <devsingh120397@gmail.com>
2018-08-05 12:29:01 -0700

    added WSIWYG

--- admin/includes/navigation_admin.php
+++ admin/includes/navigation_admin.php
@@ -1,12 +1,12 @@
 <div class="sidenav">
-    <a href="../index.php"><i class="fa fa-home" aria-hidden="true">  CMS</i></a>
-    <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-    <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
-    <a href="post.php"><i class="fa fa-fw fa-desktop"></i> View All Posts</a>
-    <a href="post.php?source=add"><i class="fa fa-plus-circle"></i>   Add Post</a>
-    <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
-    <a href="users.php?action=print"><i class="fa fa-globe"></i>  All users</a>
-    <a href="users.php?action=add"><i class="fa fa-plus-circle"> </i>       Add Users</a>
-    <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
-    <a href="../admin/includes/logout.php"><i class="fa fa-sign-out"></i> Log Out</a>
+    <a href="../index.php"><i class="icon fa fa-home" aria-hidden="true"></i>CMS</a>
+    <a href="index.php"><i class="icon fa fa-fw fa-dashboard"></i>Dashboard</a>
+    <a href="categories.php"><i class="icon fa fa-fw fa-wrench"></i>Categories</a>
+    <a href="post.php"><i class="icon fa fa-fw fa-desktop"></i>View All Posts</a>
+    <a href="post.php?source=add"><i class="icon fa fa-plus-circle"></i>Add Post</a>
+    <a href="comments.php"><i class="icon fa fa-fw fa-file"></i>Comments</a>
+    <a href="users.php?action=print"><i class="icon fa fa-globe"></i>All users</a>
+    <a href="users.php?action=add"><i class="icon fa fa-plus-circle"> </i>Add Users</a>
+    <a href="profile.php"><i class="icon fa fa-fw fa-dashboard"></i>Profile</a>
+    <a href="../admin/includes/logout.php"><i class="icon fa fa-sign-out"></i>Log Out</a>
 </div>
